6f3732f379d25eac3f3fb70dde849144cee424c0
ZhuHong <hong.zhu@ucarinc.com>
2018-08-05 17:16:47 +0800

    add file

---
+++ HGCommon/HGPerson.h
@@ -0,0 +1,13 @@
+//
+//  HGPerson.h
+//  AotuDev
+//
+//  Created by ZhuHong on 2018/8/2.
+//  Copyright © 2018年 CoderHG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HGPerson : NSObject
+
+@end
---
+++ HGCommon/HGPerson.m
@@ -0,0 +1,17 @@
+//
+//  HGPerson.m
+//  AotuDev
+//
+//  Created by ZhuHong on 2018/8/2.
+//  Copyright © 2018年 CoderHG. All rights reserved.
+//
+
+#import "HGPerson.h"
+
+@implementation HGPerson
+
+- (void)dealloc {
+    NSLog(@"我的离开, 是你的期待");
+}
+
+@end
